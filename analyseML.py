cbeb52d342ab1c2cd70eb9e0e385421c0a97d5f6
jb_frhl <72656353+sirthirrygolooo@users.noreply.github.com>
2024-10-22 16:59:59 +0200

    hehe

--- analyseML.py
+++ analyseML.py
@@ -11,28 +11,34 @@ EXPORT_PATH_TESTS_DEC = './CICIoV2024/tests/decimal/'
 EXPORT_PATH_TESTS_BIN = './CICIoV2024/tests/binary/'
 
 # Modèles utilisés
+# models = {
+#     "XGBoost": XGBClassifier(use_label_encoder=False,
+#                               eval_metric='logloss',
+#                               n_estimators=200,
+#                               learning_rate=0.1,
+#                               max_depth=5,
+#                               colsample_bytree=0.8,
+#                               subsample=0.8
+#                               ),
+#     "LightGBM": LGBMClassifier(n_estimators=200,
+#                                learning_rate=0.1,
+#                                max_depth=5,
+#                                num_leaves=50,
+#                                colsample_bytree=0.8,
+#                                subsample=0.8
+#                                ),
+#     "ExtraTrees": ExtraTreesClassifier(n_estimators=200,
+#                                        max_depth=20,
+#                                        min_samples_split=5,
+#                                        min_samples_leaf=2,
+#                                        max_features='sqrt'
+#                                        )
+# }
+
 models = {
-    "XGBoost": XGBClassifier(use_label_encoder=False,
-                              eval_metric='logloss',
-                              n_estimators=200,
-                              learning_rate=0.1,
-                              max_depth=5,
-                              colsample_bytree=0.8,
-                              subsample=0.8
-                              ),
-    "LightGBM": LGBMClassifier(n_estimators=200,
-                               learning_rate=0.1,
-                               max_depth=5,
-                               num_leaves=50,
-                               colsample_bytree=0.8,
-                               subsample=0.8
-                               ),
-    "ExtraTrees": ExtraTreesClassifier(n_estimators=200,
-                                       max_depth=20,
-                                       min_samples_split=5,
-                                       min_samples_leaf=2,
-                                       max_features='sqrt'
-                                       )
+    "XGBoost": XGBClassifier(use_label_encoder=False, eval_metric='logloss'),
+    "LightGBM": LGBMClassifier(),
+    "ExtraTrees": ExtraTreesClassifier()
 }
 
 # ################################ Decimal ################################
@@ -50,12 +56,13 @@ models = {
 
 # aggregated_df.to_csv(f'{EXPORT_PATH_TESTS_DEC}aggregated_df.csv', index=False)
 
-# X_train,X_test,y_train,y_test = prepare_data(decimal.aggregated_df)
-# train_and_evaluate(models, X_train, X_test, y_train, y_test, "Decimal Aggregated")
+X_train,X_test,y_train,y_test = prepare_data(decimal.aggregated_df)
+train_and_evaluate(models, X_train, X_test, y_train, y_test, "Decimal Aggregated")
 
 # X_train,X_test,y_train,y_test = prepare_data(binary.aggregated_df)
 # train_and_evaluate(models, X_train, X_test, y_train, y_test, "Binary Aggregated")
 
-X_train, X_test, y_train, y_test = prepare_data(decimal.aggregated_df)
-train_and_evaluate(models['XGBoost'], X_train, X_test, y_train, y_test, "Dec aggregated")
-plot_normalized_confusion_matrix(model=models['XGBoost'],X_test=X_test,y_test=y_test, model_name="XGBoost")
+# X_train, X_test, y_train, y_test = prepare_data(decimal.aggregated_df)
+# train_and_evaluate(models['XGBoost'], X_train, X_test, y_train, y_test, "Dec aggregated")
+# print('ok')
+# plot_normalized_confusion_matrix(model=models['XGBoost'],X_test=X_test,y_test=y_test, model_name="XGBoost")
